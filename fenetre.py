c129caeb7766cf42ab0afefd99b6828b137c4a8e
DavidCGG <2372916@cgodin.qc.ca>
2025-01-24 12:32:15 -0500

    Environement virtuel ajouté (.venv) avec arcade 3.0.0.dev42. Création fichier fenetre.py pour une fenetre de base. Fait avec PyCharm

---
+++ fenetre.py
@@ -0,0 +1,27 @@
+import arcade
+from pyglet.event import EVENT_HANDLE_STATE
+
+fenetre_largeur=1000
+fenetre_hauteur=500
+fenetre_titre="Titre"
+
+class VueJeu(arcade.Window):
+    def __init__(self):
+        super().__init__(fenetre_largeur,fenetre_hauteur,fenetre_titre)
+        self.background_color=arcade.csscolor.CORNFLOWER_BLUE
+
+    def setup(selfself):
+        #Set up le jeu ici. Call pour redemarrer le jeu.
+        pass
+
+    def on_draw(self):
+        self.clear()
+        #code pour draw ici
+
+def main():
+    fenetre=VueJeu()
+    fenetre.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
