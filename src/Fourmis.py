1059e65245300e1424f9fe04eb414ef83349bdfc
Strykerxman <strykerxman@gmail.com>
2025-01-25 15:57:30 -0500

    ajout de Fourmis.py dans SRC. debut tres simple de fonctionnalite

---
+++ src/Fourmis.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Fourmis(ABC):
+    def __init__(self, hp: int, atk: int):
+        self.hp = hp
+        self.atk = atk
+
+    @abstractmethod
+    def attack(self, other):
+        pass
+
+class Ouvriere(Fourmis):
+    def __init__(self):
+        super().__init__(hp=10, atk=2)
+
+    def attack(self, other):
+        other.hp -= self.atk
+
+class Soldat(Fourmis):
+    def __init__(self):
+        super().__init__(hp=25, atk=5)
+
+    def attack(self, other):
+        other.hp -= self.atk
