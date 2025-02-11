00b57ee730d52cbbacf4af313f874b5f73565443
Strykerxman <strykerxman@gmail.com>
2025-02-11 17:34:28 -0500

    EXPERIMENTAL - je ne fais que tester l'animation pour l'instant, rien de final, le code est degueulasse

--- src/Fourmis.py
+++ src/Fourmis.py
@@ -2,6 +2,7 @@ import math
 import random
 from abc import ABC, abstractmethod
 import pygame
+from config import WIDTH, HEIGHT
 
 
 
@@ -19,12 +20,19 @@ class Fourmis(ABC):
         self.facing = 0 # 0 bright
         self.hp = hp
         self.atk = atk
+        self.width = 0
+        self.height = 0
+        self.pause_timer = 0
 
     @abstractmethod
     def attack(self, other):
         pass
 
-    def update(self):
+    def update(self, dt):
+        if self.pause_timer > 0:
+            self.pause_timer -= dt
+            return
+
         if self.type_mouvement == "random":
             self.random_mouvement()
         elif self.type_mouvement == "pathfind":
@@ -35,12 +43,15 @@ class Fourmis(ABC):
     def random_mouvement(self):
         if self.centre_x == self.target_x and self.centre_y == self.target_y:
             self.set_nouv_target()
+            self.pause_timer = random.uniform(800, 2000)
 
         self.goto_target()
 
     def set_nouv_target(self):
+
         angle = random.uniform(0, 2*math.pi)
-        distance = random.uniform(20,100)
+        distance = random.uniform(40, 150)
+
         self.target_x = self.centre_x + distance*math.cos(angle)
         self.target_y = self.centre_y + distance*math.sin(angle)
 
@@ -52,9 +63,15 @@ class Fourmis(ABC):
         if distance > self.speed:
             self.centre_x += self.speed*dx/distance
             self.centre_y += self.speed*dy/distance
-        else:
+
+        elif distance <= self.speed:
+            print("Arrived")
             self.centre_x = self.target_x
             self.centre_y = self.target_y
+        if dx > 0:
+            self.facing = 0
+        else:
+            self.facing = 1
 
     def pathfind_mouvement(self):
         pass
@@ -87,22 +104,35 @@ class FourmisSprite(pygame.sprite.Sprite):
         self.frame_duration = frame_duration
         self.current_frame = 0
         self.timer = 0
-        self.frames = self.extract_frames()
-        self.image = self.frames[self.current_frame]
+        self.frames_LEFT = self.extract_frames()
+        self.frames_RIGHT = self.extract_frames(flipped=True)
+        self.image = self.frames_LEFT[self.current_frame]
         self.rect = self.image.get_rect(center=(self.fourmis.centre_x, self.fourmis.centre_y))
 
-    def extract_frames(self):
+    def extract_frames(self, flipped=False):
         frames = []
+        frame = None
         for i in range(self.num_frames):
             frame = self.spritesheet.subsurface(pygame.Rect(i*self.frame_width, 0, self.frame_width, self.frame_height))
             frame = pygame.transform.scale(frame, (int(self.frame_width*self.fourmis.scale), int(self.frame_height*self.fourmis.scale)))
+            if flipped:
+                frame = pygame.transform.flip(frame, True, False)
             frames.append(frame)
+
+        self.fourmis.width = frame.get_width()
+        self.fourmis.height = frame.get_height()
         return frames
 
     def update(self, dt):
-        self.fourmis.update()
+        self.fourmis.update(dt)
+        if self.fourmis.pause_timer > 0:
+            return
         self.timer += dt
         while self.timer > self.frame_duration:
             self.timer -= self.frame_duration
             self.current_frame = (self.current_frame + 1) % self.num_frames
-            self.image = self.frames[self.current_frame]
+        if self.fourmis.facing == 0:
+            self.image = self.frames_RIGHT[self.current_frame]
+        else:
+            self.image = self.frames_LEFT[self.current_frame]
+        self.rect.center = (self.fourmis.centre_x, self.fourmis.centre_y)
