e28b243181369e8a196271bdee53d1a6af6c31b4
vincentGagnon06 <vincegagnon2000@gmail.com>
2025-04-17 14:39:48 -0400

    Retrait de l'interface TKinter dans generationNid

--- src/testGenerationNid.py
+++ src/testGenerationNid.py
@@ -101,94 +101,44 @@ def convertir_coord(graphe: pg.Graph, scale = 50) -> None:
         if salle != salle_min:
             salle.noeud.coord[1] += 75
         
-def interface() -> None:
-    def initialiser() -> None:
-        root = tk.Tk()
-        root.geometry("500x350")
-        root.title("Configuration des paramètres de génération de graphe")
+def run() -> None:
+    running = True
 
-        labels = ["Nombre de noeuds", "Nombre d'itérations des forces", "Chance de connection", "Taux médianne", "Médianne Initiale", "Taux écart-type", "Écart-type initial"]
-        default_values: list[float] = [nb_noeuds_cible, nb_iter_forces, connect_chance, taux_mean, initial_mean, taux_std_dev, initial_std_dev]
-        entry_vars: list[tk.StringVar] = []
+    screen = init()
+    graphe: pg.Graph = generer_graphe(
+        (nb_noeuds_cible, taux_mean, initial_mean, taux_std_dev, initial_std_dev),
+        connect_chance,
+        nb_iter_forces
+    )
+    convertir_coord(graphe)
 
-        for i, label in enumerate(labels):
-            tk.Label(root, text=label).grid(row=i, column=0, padx=10, pady=5)
-            
-            var = tk.StringVar(value=str(default_values[i]))
-            var.trace_add("write", lambda name, index, mode, i=i, v=var: modifier_valeur(i, v))
+    camera = Camera(SCREEN_WIDTH, SCREEN_HEIGHT, MAP_LIMIT_X, MAP_LIMIT_Y)
 
-            entry = tk.Entry(root, textvariable=var)
-            entry.grid(row=i, column=1, padx=10, pady=5)
-            
-            entry_vars.append(var)
+    while running:
+        #TODO comparer à handle_event dans carte
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        tk.Button(root, text="Générer des graphes", command=afficher_graphe).grid(row=7, column=0, columnspan=2, pady=10)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Clic gauche
+                    camera.start_drag(*event.pos)
+                elif event.button == 4:  # Molette haut
+                    camera.zoom_camera(*event.pos, "in")
+                elif event.button == 5:  # Molette bas
+                    camera.zoom_camera(*event.pos, "out")
 
-        root.mainloop()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    camera.stop_drag()
 
-    def modifier_valeur(index: int, var: tk.StringVar):
-        global nb_noeuds_cible, nb_iter_forces, connect_chance, taux_mean, initial_mean, taux_std_dev, initial_std_dev
+            elif event.type == pygame.MOUSEMOTION:
+                camera.drag(*event.pos)
 
-        try:
-            new_value = float(var.get())
-        except ValueError:
-            return
+        draw(screen, graphe, camera)
+        clock.tick(60)
 
-        if index == 0:
-            nb_noeuds_cible = int(new_value)
-        elif index == 1:
-            nb_iter_forces = int(new_value)
-        elif index == 2:
-            connect_chance = new_value
-        elif index == 3:
-            taux_mean = new_value
-        elif index == 4:
-            initial_mean = new_value
-        elif index == 5:
-            taux_std_dev = new_value
-        elif index == 6:
-            initial_std_dev = new_value
-
-    def afficher_graphe() -> None:
-        running = True
-
-        screen = init()
-        graphe: pg.Graph = generer_graphe(
-            (nb_noeuds_cible, taux_mean, initial_mean, taux_std_dev, initial_std_dev),
-            connect_chance,
-            nb_iter_forces
-        )
-        convertir_coord(graphe)
-
-        camera = Camera(SCREEN_WIDTH, SCREEN_HEIGHT, MAP_LIMIT_X, MAP_LIMIT_Y)
-
-        while running:
-            #TODO comparer à handle_event dans carte
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:  # Clic gauche
-                        camera.start_drag(*event.pos)
-                    elif event.button == 4:  # Molette haut
-                        camera.zoom_camera(*event.pos, "in")
-                    elif event.button == 5:  # Molette bas
-                        camera.zoom_camera(*event.pos, "out")
-
-                elif event.type == pygame.MOUSEBUTTONUP:
-                    if event.button == 1:
-                        camera.stop_drag()
-
-                elif event.type == pygame.MOUSEMOTION:
-                    camera.drag(*event.pos)
-
-            draw(screen, graphe, camera)
-            clock.tick(60)
-
-        pygame.quit()
+    pygame.quit()
     
-    initialiser()
-
 if __name__ == "__main__":
-    interface()
+    run()
